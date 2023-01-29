341a19cf0b5df6e197597beae2ffcfcfb72d335e
Tomasz Białobrzeg <tomasz.bialobrzeg@ceferty.pl>
2023-01-29 21:18:25 +0100

    less pipes, better NextPage functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';  
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DBService } from './db.service';
 import { TransactionsComponent } from './transactions/transactions.component';
-import { FilterDescriptionPipe } from './filter-description.pipe';
-import { PriceRangePipe } from './price-range.pipe';
-import { TimeFilterPipe } from './time-filter.pipe';
 import { PrintDatePipe } from './print-date.pipe';
 import { PrintPricePipe } from './print-price.pipe';
 import { CurrentPagePipe } from './current-page.pipe';
@@ -20,9 +17,6 @@ import { TransactionComponent } from './transaction/transaction.component';
   declarations: [
     AppComponent,
     TransactionsComponent,
-    FilterDescriptionPipe,
-    PriceRangePipe,
-    TimeFilterPipe,
     PrintDatePipe,
     PrintPricePipe,
     CurrentPagePipe,
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DBService, Transaction } from '../db.service';
-import { TimeFilter } from "../time-filter.pipe";
+import { DBService, isTransactionPriceValid, isTransactionTimeValid, Transaction } from '../db.service';
+
+
+export enum TimeFilter {
+  WHOLE_TIME = "whole time",
+  THIS_YEAR = "this year",
+  THIS_MONTH = "this month",
+  PREVIOUS_MONTH = "previous month"
+}
 
 @Component({
   selector: 'app-transactions',
@@ -10,26 +17,118 @@ import { TimeFilter } from "../time-filter.pipe";
 export class TransactionsComponent {
 
   public readonly PAGE_SIZE = 5;
-  public readonly INITIAL_CHUNK_SIZE = 100;
   public readonly TimeFilter = TimeFilter;
   public timeFilter = TimeFilter.WHOLE_TIME;
   public descriptionQuery: string = "";
   public priceFrom: string = "";
   public priceTo: string = "";
   public pageIndex = 0;
-
+  private allTransactions: Transaction[] = [];
+  private filteredTransactions: Transaction[] = [];
 
   constructor(public dbService: DBService) {
-    dbService.loadTransactions(this.INITIAL_CHUNK_SIZE);
+    dbService.loadNextPage(this.PAGE_SIZE).then(() => {
+      this.updateTransactionsLists();
+    });
+  }
+
+  public getFilteredTransactions() {
+    return this.filteredTransactions;
+  }
+
+  public nexPage() {
+    const maxIndex = this.maxFilteredTransactionsPageIndex(this.filteredTransactions.length);
+
+    if (this.pageIndex == maxIndex) {
+      this.dbService.loadNextPage(this.PAGE_SIZE).then(() => {
+        this.updateTransactionsLists();
+        this.pageIndex = this.maxFilteredTransactionsPageIndex(this.filteredTransactions.length);
+      })
+      return;
+    }
+    this.pageIndex += 1;
   }
 
   public setPage(pageIndex: number) {
     this.pageIndex = pageIndex >= 0 ? pageIndex : 0;
-    this.dbService.loadTransactions((this.pageIndex + 1) * this.PAGE_SIZE);
+    this.updateTransactionsLists();
   }
 
   public resetPageSearch() {
-    if (this.descriptionQuery.length >= 3) this.pageIndex = 0;
+    if (this.descriptionQuery.length >= 3) {
+      this.pageIndex = 0;
+      this.updateTransactionsLists();
+    }
+  }
+
+  private updateTransactionsLists() {
+    this.allTransactions = this.dbService.getTransactions();
+    this.filteredTransactions = this.descriptionFilterFunc(this.allTransactions);
+    this.filteredTransactions = this.timeFilterFunc(this.filteredTransactions);
+    this.filteredTransactions = this.priceFilterFunc(this.filteredTransactions);
+
+    return this.filteredTransactions;
+  }
+
+  // Angular doesn't recommend to sort or filter in pipes
+  private descriptionFilterFunc(transactions: Transaction[]) {
+    return transactions.filter((transaction) => {
+      if (transaction == null) return false;
+      if (this.descriptionQuery == null || this.descriptionQuery.length < 3) return true;
+      if (transaction.description == null) return false;
+      return transaction.description.search(this.descriptionQuery) >= 0;
+    });
+  }
+
+  // Angular doesn't recommend to sort or filter in pipes
+  private timeFilterFunc(transactions: Transaction[]) {
+    const now = new Date();
+    const thisYear = now.getFullYear();
+    const thisMonth = now.getMonth();
+    return transactions.filter(transaction => {
+
+      if (!isTransactionTimeValid(transaction) && this.timeFilter == TimeFilter.WHOLE_TIME) return true;
+      if (!isTransactionTimeValid(transaction) && this.timeFilter != TimeFilter.WHOLE_TIME) return false;
+
+      const transactionDate = new Date(transaction.metadata.subscription_created);
+      const transactionYear = transactionDate.getFullYear();
+      const transactionMonth = transactionDate.getMonth();
+
+      switch (this.timeFilter) {
+        case TimeFilter.THIS_YEAR: {
+          return transactionYear == thisYear;
+        }
+        case TimeFilter.THIS_MONTH: {
+          return transactionMonth == thisMonth && transactionYear == thisYear;
+        }
+        case TimeFilter.PREVIOUS_MONTH: {
+          if (thisMonth > 0) return transactionMonth == thisMonth - 1 && transactionYear == thisYear;
+          return transactionMonth == 11 && transactionYear == thisYear - 1;
+        }
+        default: {
+          return true;
+        }
+      }
+    });
+  }
+
+  // Angular doesn't recommend to sort or filter in pipes
+  private priceFilterFunc(transactions: Transaction[]) {
+    return transactions.filter(transaction => {
+      if (!isTransactionPriceValid(transaction)) return true
+      let fromNumber = 0;
+      let toNumber = Infinity;
+      if (this.priceFrom != "") fromNumber = parseFloat(this.priceFrom);
+      if (this.priceTo != "") toNumber = parseFloat(this.priceTo);
+      let transactionPrice = parseFloat(transaction.metadata.price_eur);
+      return transactionPrice <= toNumber && transactionPrice >= fromNumber;
+    })
+  }
+
+  private maxFilteredTransactionsPageIndex(transactionsNumber: number) {
+    let index = Math.floor((transactionsNumber - 1) / this.PAGE_SIZE);
+    if (index < 0) index = 0;
+    return index;
   }
 
 }
